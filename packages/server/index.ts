cc7dcbb6397c74e001bfce327b3c89a4a428a6b0
GeorgeLympero <glympe77@gmail.com>
2025-08-24 08:42:51 +0300

    connect backend and frontend

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -9,6 +9,12 @@ const port = process.env.port || 3000;
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
 });
+app.get('/api/hello', (req: Request, res: Response) => {
+  console.log('Received request at /api/hello');
+  res.json({
+    message: 'Hello from the API!',
+  });
+});
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
